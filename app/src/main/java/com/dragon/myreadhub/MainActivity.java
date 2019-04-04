3345ecc057554cf61bdf588343615fe44aa1d8e9
gonghe <gongheoffice@163.com>
2019-04-04 12:56:37 +0800

    第一次提交

---
+++ app/src/main/java/com/dragon/myreadhub/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dragon.myreadhub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
